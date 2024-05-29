01a69eb1a3bfdc5725d792d7eb217257441dd3eb
Ashish kumar <102891997+erashish21@users.noreply.github.com>
2024-05-29 15:04:54 +0530

    database connect

---
+++ config/database.js
@@ -0,0 +1,32 @@
+// config/database.js
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl:
+        process.env.DATABASE_SSL === "true"
+          ? { rejectUnauthorized: false }
+          : false,
+    },
+  }
+);
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+module.exports = { sequelize, connectDB };
--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { connectDB } = require("./config/database");
 const dotenv = require('dotenv');
+
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+connectDB(); 
+
+app.get("/test", async (req, res) => {
+  try {
+    const result = await Sequelize.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
